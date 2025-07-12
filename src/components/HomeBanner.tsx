6d801ea6c7adb1f36b07483e286591a4e26e3541
SamGuan3 <3462343313@qq.com>
2025-07-12 12:59:16 +0800

    调整首页欢迎语到左上角

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -5,9 +5,23 @@ import React from "react";
  * @returns {JSX.Element}
  */
 const HomeBanner = () => (
-  <div>
-    {/* 你的头像和背景部分 */}
-    <h1>欢迎来到呱鲨的大学生活</h1>
+  <div style={{ position: "relative", width: "100%", height: "100%" }}>
+    {/* 左上角欢迎语 */}
+    <h1
+      style={{
+        position: "absolute",
+        top: 30,
+        left: 30,
+        margin: 0,
+        color: "#fff",
+        fontSize: "2.5rem",
+        zIndex: 2,
+        fontWeight: "bold"
+      }}
+    >
+      欢迎来到呱鲨的大学生活
+    </h1>
+    {/* 头像、抖音号、B站账号等内容可以继续放在合适位置 */}
     <div>
       抖音号：
       <a
