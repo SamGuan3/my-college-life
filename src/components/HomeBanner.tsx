0c3176d5abac1539fed6c39625bdcbd6f8e43de3
SamGuan3 <3462343313@qq.com>
2025-07-12 16:23:51 +0800

    修复首页头像和名字不显示及排版重叠问题

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,4 +1,4 @@
-
+import Banner from './Banner';
 
 /**
  * 首页欢迎横幅组件
@@ -6,43 +6,47 @@
  */
 const HomeBanner = () => (
   <div style={{ position: "relative", width: "100%", height: "100%" }}>
-    {/* 左上角欢迎语 */}
-    <h1
-      style={{
-        position: "absolute",
-        top: 30,
-        left: 30,
+    {/* 头像和名字（Banner组件） */}
+    <Banner />
+
+    {/* 左上角欢迎语和账号信息 */}
+    <div style={{
+      position: "absolute",
+      top: 30,
+      left: 30,
+      color: "#fff",
+      zIndex: 2,
+      textAlign: "left"
+    }}>
+      <h1 style={{
         margin: 0,
-        color: "#fff",
         fontSize: "2.5rem",
-        zIndex: 2,
         fontWeight: "bold"
-      }}
-    >
-      欢迎来到呱鲨的大学生活
-    </h1>
-    {/* 头像、抖音号、B站账号等内容可以继续放在合适位置 */}
-    <div>
-      抖音号：
-      <a
-        href="https://www.douyin.com/user/G3462343313"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ color: "red" }}
-      >
-        G3462343313
-      </a>
-    </div>
-    <div>
-      Bilibili账号：
-      <a
-        href="https://space.bilibili.com/你的UID"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ color: "blue" }}
-      >
-        _呱呱呱鲨
-      </a>
+      }}>
+        欢迎来到呱鲨的大学生活
+      </h1>
+      <div>
+        抖音号：
+        <a
+          href="https://www.douyin.com/user/G3462343313"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "red" }}
+        >
+          G3462343313
+        </a>
+      </div>
+      <div>
+        Bilibili账号：
+        <a
+          href="https://space.bilibili.com/你的UID"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "blue" }}
+        >
+          _呱呱呱鲨
+        </a>
+      </div>
     </div>
   </div>
 );
