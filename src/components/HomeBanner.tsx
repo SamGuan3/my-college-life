437772c40db1a51862329c5c9ebf1447610ab61c
SamGuan3 <3462343313@qq.com>
2025-07-12 12:55:19 +0800

    修复首页头像和名字不显示问题

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,4 +1,4 @@
-
+import Banner from './Banner';
 
 /**
  * 首页欢迎横幅组件
@@ -6,6 +6,7 @@
  */
 const HomeBanner = () => (
   <div>
+    <Banner />
     <h1>欢迎来到呱鲨的大学生活</h1>
     <div>抖音号：<span style={{color: "red"}}>G3462343313</span></div>
     <div>Bilibili账号：<span style={{color: "blue"}}>_呱呱呱鲨</span></div>
