ee30c729d5488dbc755c0b5a87e005a23dd7cec0
SamGuan3 <3462343313@qq.com>
2025-07-14 19:26:01 +0800

    feat: 更新仪表盘小部件及样式优化

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import GridLayout from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
